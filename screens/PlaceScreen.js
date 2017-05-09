3c3a85cf2f27fc87ad3776fd202ea0b753289a55
damisgarcia <damisgarcia@gmail.com>
2017-05-09 15:20:29 -0300

    Add External links for Maps, and Site

--- screens/PlaceScreen.js
+++ screens/PlaceScreen.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 import NavigationStyle from '../constants/NavigationStyle';
 import {
   Container,
   Content,
   Body,
+  Row,
   Button,
   Icon,
 } from 'native-base';
@@ -20,6 +21,7 @@ export default class PlaceScreen extends React.Component {
   render() {
     const { state } = this.props.navigation;
     const { place } = state.params
+    console.log(place.latitude, place.longitude)
     return (
       <Container style={styles.container}>
         <View style={styles.pictureContainer}>
@@ -30,6 +32,14 @@ export default class PlaceScreen extends React.Component {
             <Text>{place.title}</Text>
             <Text note>{place.sumary}</Text>
             <Text note>{place.keywords}</Text>
+            <Row>
+              <Button style={styles.button} primary onPress={ ()=> Linking.openURL(place.lister_url).catch(onLinkFail) }>
+                <Text style={styles.textButton}>{'Ver no site'.toUpperCase()}</Text>
+              </Button>
+              <Button style={styles.button} light onPress={ ()=> Linking.openURL(`geo:${place.latitude},${place.longitude}?q=${place.latitude},${place.longitude}(${place.title}),`).catch(onLinkFail) }>
+                <Text>{'Ver no mapa'.toUpperCase()}</Text>
+              </Button>
+            </Row>
           </View>
         </Content>
       </Container>
@@ -37,6 +47,9 @@ export default class PlaceScreen extends React.Component {
   }
 };
 
+const onLinkFail = (err)=>{
+  console.error('An error occurred', err);
+}
 
 const styles = {
   container: {
@@ -47,8 +60,12 @@ const styles = {
     padding: 16
   },
   button: {
-    marginTop: 24,
-    marginBottom: 24
+    marginTop: 16,
+    marginBottom: 16,
+    marginRight: 8
+  },
+  textButton: {
+    color: 'white',
   },
   pictureContainer: {
     flex:1,
